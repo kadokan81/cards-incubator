28935dbb9026e7b6c51366c9cf0c7b33b23a23fb
kadokan81 <kadokan81@gmail.com>
2023-10-17 00:07:56 +0200

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
