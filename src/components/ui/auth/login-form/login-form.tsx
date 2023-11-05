d220595f0de77f0bcd183955acf800b0119ed95c
kadokan81 <kadokan81@gmail.com>
2023-11-05 14:24:54 +0100

    controlled radio buttons

--- src/components/ui/auth/login-form/login-form.tsx
+++ src/components/ui/auth/login-form/login-form.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { DevTool } from '@hookform/devtools'
@@ -9,23 +10,25 @@ import s from './login-form.module.scss'
 
 import Button from '../../button/button'
 import { ControlledCheckbox } from '../../controlled/controlled-checkbox/controlled-checkbox'
-import { TextField } from '../../text-field'
+import { ControlledRadioGroup } from '../../controlled/controlled-radio-group'
+import { ControlledTextField } from '../../controlled/controlled-text-field'
 import { Typography } from '../../typography'
 
 const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(3),
   rememberMe: z.boolean().default(false),
+  unswear: z.string(),
 })
 
 type FormValues = z.infer<typeof loginSchema>
 
 export const LoginForm = () => {
+  const [selectedOption, setSelectedOption] = useState('Option 1')
   const {
     control,
     formState: { errors },
     handleSubmit,
-    register,
   } = useForm<FormValues>({
     resolver: zodResolver(loginSchema),
   })
@@ -36,23 +39,27 @@ export const LoginForm = () => {
     console.log('🚀 ~ file: login-form.tsx:18 ~ onSubmit ~ data:', data)
   }
 
+  const handleRadioChange = (value: string) => {
+    setSelectedOption(value)
+  }
+  const radioOptions = [
+    { label: 'Option 1', value: 'Option 1' },
+    { label: 'Option 2', value: 'Option 2' },
+    { label: 'Option 3', value: 'Option 3' },
+  ]
+
   return (
     <Card className={s.loginForm}>
       <form className={s.loginForm__form} onSubmit={handleSubmit(onSubmit)}>
         <DevTool control={control} />
         <Typography as={'h1'}>Sign In</Typography>
-
-        <TextField {...register('email')} errorMessage={errors.email?.message} label={'email'} />
-        <TextField
-          {...register('password')}
-          errorMessage={errors.password?.message}
-          label={'password'}
-        />
-        {/* <Checkbox checked={value} label={'remember me'} onChange={onChange} /> */}
+        <ControlledTextField control={control} label={'email'} name={'email'} />
+        <ControlledTextField control={control} label={'password'} name={'password'} />
         <ControlledCheckbox control={control} label={'remember me'} name={'rememberMe'} />
         <Button fullWidth type={'submit'}>
           Sign Up
         </Button>
+        <ControlledRadioGroup control={control} name={'unswear'} options={radioOptions} />
       </form>
     </Card>
   )
---
+++ src/components/ui/controlled/controlled-radio-group/controlled-radio-group.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Control, FieldPath, FieldValues, useController } from 'react-hook-form'
+
+import s from './controlled-radio-group.module.scss'
+
+type ControlledRadioGroupProps<TFieldValues extends FieldValues> = {
+  control: Control<TFieldValues>
+  name: FieldPath<TFieldValues>
+  options: { label: string; value: string }[]
+}
+
+export const ControlledRadioGroup = <TFieldValues extends FieldValues>(
+  props: ControlledRadioGroupProps<TFieldValues>
+) => {
+  const {
+    field: { onChange, value },
+  } = useController({
+    control: props.control,
+    name: props.name,
+  })
+  const classNames = {
+    groupContainer: s.groupContainer,
+    optionLabel: s.optionLabel,
+  }
+
+  return (
+    <div className={classNames.groupContainer}>
+      {props.options.map((option, index) => (
+        <label className={classNames.optionLabel} key={`${index}__${option.value}`}>
+          <input
+            checked={value === option.value}
+            onChange={e => onChange(e.target.value)}
+            type={'radio'}
+            value={option.value}
+          />
+          {option.label}
+        </label>
+      ))}
+    </div>
+  )
+}
