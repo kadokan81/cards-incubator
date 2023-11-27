2bef81e5c67fa89d0febea501726c1d66492e742
kadokan81 <kadokan81@gmail.com>
2023-11-27 14:32:19 +0100

    card table

---
+++ src/pages/cards-table/cards-table.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+import { Page } from '@/components/ui/page'
+import { TableCards } from '@/components/ui/table'
+
+export const CardsTable = () => {
+  return (
+    <Page>
+      <TableCards />
+    </Page>
+  )
+}
